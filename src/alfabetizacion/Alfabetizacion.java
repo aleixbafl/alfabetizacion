7ebc84ed9ee9390435837b6842ef0979bc5d986b
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2024-04-10 18:36:51 +0200

    cionexioBD

--- src/alfabetizacion/Alfabetizacion.java
+++ src/alfabetizacion/Alfabetizacion.java
@@ -1,6 +1,19 @@
 package alfabetizacion;
+
+import loginReguistre.login;
+
 public class Alfabetizacion {
     public static void main(String[] args) {
-        
+        Alfabetizacion aplicacio =  new Alfabetizacion();
+        aplicacio.inici();
+    }
+
+    private void inici() {
+        login pantallaLogin = new login();
+        pantallaLogin.setTitle("Iniciar Sessió - Alfabetització");
+        pantallaLogin.setMinimumSize(new java.awt.Dimension(500, 600));
+        pantallaLogin.setResizable(false);
+        pantallaLogin.setLocationRelativeTo(null);
+        pantallaLogin.setVisible(true);
     }
 }
